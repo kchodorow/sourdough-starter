b9a31aa827f0ccc7d8776b25ad33ff4dea7a99dd
Kristina Chodorow <k.chodorow@gmail.com>
2020-04-19 15:04:20 -0400

    Nicer game over screen

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # TODO
 
-* Make thermometer blue/red when it's cold/hot
-* Add maturity meter
+* Add guages for all three measures
+* Add interstitials with directions
 
 Intro:
 
@@ -16,6 +16,7 @@ End game:
 
 * Dough chopper
 * Scallion pancake?
-* Boule
+* Boule?
+* Water pitcher?
 
 A box with two 1px lines as a border would look classy around text.
--- js/gameover.js
+++ js/gameover.js
@@ -6,23 +6,24 @@ class GameOver extends BaseStep {
     create(data) {
         this.cameras.main.setBackgroundColor(BACKGROUND_COLOR);
         const title = this.add.text(400, 150, '', this._textStyle);
+        let statsStr = ''
         if (data.win) {
             title.setText(
                 'Congratulations!\nYou successfully matured your stater.');
+            statsStr = (
+                `It took ${data.elapsedTime} seconds to reach maturity.\n` +
+                `Scallion pancakes: ${data.discard}`);
             // TODO: add some particle effects.
         } else {
             title.setText(`Game over\nYour starter ${data.causeOfDeath}.`);
+            statsStr = (
+                `Your starter survived ${data.elapsedTime} seconds\n` +
+                `Scallion pancakes: ${data.discard}`);
         }
-        title.setOrigin(.5, 0);
-        title.setAlign('center');
-        const statsStr = `Stats:\n` +
-            `Elapsed time: ${data.elapsedTime} seconds\n` +
-            `Scallion pancakes: ${data.discard}`;
-        const stats = this.add.text(
-            400, 250, statsStr, this._textStyle);
-        stats.setFontSize(20);
-        stats.setOrigin(.5, 0);
-        stats.setAlign('center');
+        title.setOrigin(.5, 0).setFontSize('42px').setAlign('center');
+
+        const stats = this.add.text(400, 275, statsStr, this._textStyle);
+        stats.setOrigin(.5, 0).setFontSize('26px').setAlign('center');
 
         // TODO: add try again? button.
     }
