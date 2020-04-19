a100ca829a67673cf5049be30d67313c2be2301f
Kristina Chodorow <k.chodorow@gmail.com>
2020-04-19 11:41:33 -0400

    Break up create method

--- js/step2.js
+++ js/step2.js
@@ -26,70 +26,13 @@ class Step2 extends BaseStep {
         this._thermometer = this.add.text(780, 580, '68°', this._textStyle);
         this._thermometer.setOrigin(1, 1).setAlign('right').setFontSize(22);
 
-        player = this.physics.add.sprite(data.x, data.y, 'starter');
-        player.setBounce(0.2);
-        player.setCollideWorldBounds(true);
-        player.properties = {
-            weight: 20.0,
-            fermented: 5.0,
-            temperature: 68,
-        };
-        // Add debug.
-        // TODO: remove.
-        this._debug = this.add.text(
-            10, 10, `${player.properties.fermented}/${player.properties.weight}`, this._textStyle);
-
-
-        this.anims.create({
-            key: 'left',
-            frames: this.anims.generateFrameNumbers(
-                'starter', {start: 4, end: 6}),
-            frameRate: 10,
-            repeat: -1
-        });
-
-        this.anims.create({
-            key: 'turn',
-            frames: [{key: 'starter', frame: 0}],
-            frameRate: 20
-        });
-
-        this.anims.create({
-            key: 'right',
-            frames: this.anims.generateFrameNumbers(
-                'starter', {start: 1, end: 3}),
-            frameRate: 10,
-            repeat: -1
-        });
+        const hotspot = this.addHotspot();
+        const player = this.addPlayer(data)
+        this.physics.add.overlap(
+            player, hotspot, this.incrementTemp, null, this);
 
         // User input.
         cursors = this.input.keyboard.createCursorKeys();
-        
-        // Add warm spot.
-        const hot_spot_x = 4; //Math.floor(Math.random() * game.config.width / 50);
-        const hot_spot_y = 8; //Math.floor(Math.random() * game.config.height / 50);
-        const hotspot = this.physics.add.group({
-            key: 'hotspot',
-            repeat: 9,
-        });
-        hotspot.temperature = 140;
-        Phaser.Actions.GridAlign(hotspot.getChildren(), {
-            width: 3,
-            height: 3,
-            cellWidth: 50,
-            cellHeight: 50,
-            x: 150,
-            y: 350,
-        });
-        let tween = this.tweens.add({
-            targets: hotspot.getChildren(),
-            duration: 3000,
-            alpha: {start: .5, to: 1},
-            repeat: -1,
-            yoyo: true,
-        });
-        this.physics.add.overlap(
-            player, hotspot, this.incrementTemp, null, this);
     }
 
     update() {
@@ -127,6 +70,70 @@ class Step2 extends BaseStep {
 
         this.moveTempTowards(68);
         this.updateFermetation();
+    }
+
+    addPlayer(data) {
+        player = this.physics.add.sprite(data.x, data.y, 'starter');
+        player.setBounce(0.2);
+        player.setCollideWorldBounds(true);
+        player.properties = {
+            weight: 20.0,
+            fermented: 5.0,
+            temperature: 68,
+        };
+        // Add debug.
+        // TODO: remove.
+        this._debug = this.add.text(
+            10, 10, 
+            `${player.properties.fermented}/${player.properties.weight}`, 
+            this._textStyle);
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers(
+                'starter', {start: 4, end: 6}),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'turn',
+            frames: [{key: 'starter', frame: 0}],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers(
+                'starter', {start: 1, end: 3}),
+            frameRate: 10,
+            repeat: -1
+        });
+        return player;
+    }
+
+    addHotspot() {
+        const hotspot = this.physics.add.group({
+            key: 'hotspot',
+            repeat: 9,
+        });
+        hotspot.temperature = 140;
+        Phaser.Actions.GridAlign(hotspot.getChildren(), {
+            width: 3,
+            height: 3,
+            cellWidth: 50,
+            cellHeight: 50,
+            x: 150,
+            y: 350,
+        });
+        let tween = this.tweens.add({
+            targets: hotspot.getChildren(),
+            duration: 3000,
+            alpha: {start: .5, to: 1},
+            repeat: -1,
+            yoyo: true,
+        });
+        return hotspot;
     }
 
     incrementTemp(player, hotspot) {
